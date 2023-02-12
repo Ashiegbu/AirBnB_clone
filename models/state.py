334256a21c4e1c49e8f6621a99f5ddb1577e509b
Ashiegbu <47593584+Ashiegbu@users.noreply.github.com>
2023-02-12 13:42:53 +0100

    Classes inheriting from base_model.BaseModel
    Classes inheriting from base_model.BaseModel

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ State class """
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
+    """
+
     name = ""
