36e168d3db835adb94e3210cf4d2639dc6e976e8
Ashiegbu <47593584+Ashiegbu@users.noreply.github.com>
2023-02-12 13:41:16 +0100

    Classes inheriting from base_model.BaseModel
    Classes inheriting from base_model.BaseModel

--- models/review.py
+++ models/review.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
+    """Represent a review.
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
     text = ""
