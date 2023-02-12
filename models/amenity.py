ffcbe5585eee411f92bbbdf18033d88285899896
Ashiegbu <47593584+Ashiegbu@users.noreply.github.com>
2023-02-12 13:33:31 +0100

    Classes inheriting from base_model.BaseModel
    Classes inheriting from base_model.BaseModel

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
     name = ""
