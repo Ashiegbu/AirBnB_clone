ef982dd9c1e88a466b086019e422e00fed7ccba7
Ashiegbu <47593584+Ashiegbu@users.noreply.github.com>
2023-02-12 13:44:28 +0100

    Classes inheriting from base_model.BaseModel
    Classes inheriting from base_model.BaseModel

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """Represent a User.
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
