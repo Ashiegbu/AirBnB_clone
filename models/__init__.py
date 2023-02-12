2c3b1f4dceff37df8b9501f65bb3a0337e92e9d8
Ashiegbu <chibuch@gmail.com>
2023-02-12 04:30:42 -0800

    Bug fixed

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
 
 
